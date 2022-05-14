9b1cdfab64de226bc39360f8e5b52c0706b22082
new name <m.azims1396@gmail.com>
2022-05-14 17:23:50 +0430

    how to read a view from rout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,11 +3,12 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>learnlaravel</title>
+    <title>learn laravel</title>
     <link rel="stylesheet" type="text/css" href="/app.css">
     <script type="text/javascript" src="/app.js"></script>
 </head>
 <body>
-<h1>learn the laravel</h1>
+<h1>learn the Laravel</h1>
+
 </body>
 </html>
